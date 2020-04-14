6a06efdbdc7a9bccb5368e88756cbccfb6bfcaf9
Mateus Bueno <mateusbueno382@gmail.com>
2020-04-14 04:54:05 -0300

    final result with all tests passed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TotalBalance {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +24,40 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TotalBalance {
+    return { transactions: this.transactions, balance: this.getBalance() };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (prevIncome, currentTransaction) =>
+        currentTransaction.type === 'income'
+          ? prevIncome + currentTransaction.value
+          : prevIncome,
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (prevOutcome, currentTransaction) =>
+        currentTransaction.type === 'outcome'
+          ? prevOutcome + currentTransaction.value
+          : prevOutcome,
+      0,
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
